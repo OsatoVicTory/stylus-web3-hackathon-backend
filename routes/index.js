ee78cc1a9f71766006c6a499c355e9e562748d3f
Osato <osatohanmenogbeide1@gmail.com>
2025-04-18 15:00:35 +0100

    added pinata for ipfs

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../controllers/multer");
 const cloudinary = require("../controllers/upload");
+const pinata = require("../controllers/pinata");
 
 router.get('/get_data', cloudinary.getData);
 
@@ -10,5 +11,7 @@ router.get("/delete_uploaded_file/:public_id/:resource_type", cloudinary.deleteU
 router.post("/upload_contents_file", upload.single('file'), cloudinary.uploadContentsFile);
 
 router.post("/upload_users_file", upload.single('file'), cloudinary.uploadUsersFile);
+
+router.get('/presigned_url', pinata.uploadContentsFile);
 
 module.exports = router;
